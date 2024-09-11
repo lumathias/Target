437db277ace2ba6b1af4156ec41e0047c51aee04
Luisa Mathias <86033613+lumathias@users.noreply.github.com>
2024-09-11 14:09:03 -0300

    endfib

--- fibonacci.py
+++ fibonacci.py
@@ -3,8 +3,8 @@ será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34.
 escreva um programa na linguagem que desejar onde, informado um número, ele calcule a 
 sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
 
-IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
-"""
+IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua 
+preferência ou pode ser previamente definido no código """
 
 def is_fibonacci(num):
   a, b = 0, 1 
