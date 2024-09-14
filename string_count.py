714da531d0f31bf650ddd6f99312a3ff1eacdcd7
Luisa Mathias <86033613+lumathias@users.noreply.github.com>
2024-09-13 21:19:16 -0300

    updates

--- string_count.py
+++ string_count.py
@@ -4,18 +4,18 @@ IMPORTANTE: Essa string pode ser informada através de qualquer entrada de sua
 preferência ou pode ser previamente definida no código. """
 
 def conta_a(texto):
-  cont = 0
-  found = False
+  cont = 0 # conta as vezes que 'a' aparece
+  found = False # boleano para checar se 'a' aparece
 
-  for caractere in texto:
-    if caractere.lower() == 'a':
+  for caractere in texto: 
+    if caractere.lower() == 'a': # checa se o char é 'a'
       cont += 1
       found = True
   if found:
-    print(f"A letra 'a' foi found {cont} vezes na string.")
+    print(f"A letra 'a' foi encontrada {cont} vezes na string.")
   else:
-    print("A letra 'a' não foi found na string.")
+    print("A letra 'a' não foi encontrada na string.")
 
-# userinput = "Aprender programação pode ser divertido."
-userinput = input("Digite uma frase: ")
-conta_a(userinput)
+# userinput = "Aprender programação pode ser bastante divertido."
+userinput = input("Digite algo > ")
+conta_a(userinput) 
